1e768b879143ba9e49913117951c5e808088b3d7
Takumaron <tennis.takumax@gmail.com>
2023-10-22 15:47:06 +0700

    Feature shard-concurrency

---
+++ shard-concurrency/README.md
@@ -0,0 +1,25 @@
+# shard-concurrency
+
+シャード単位で並列実行するマイグレーションのサンプルです。
+
+### 実行結果
+
+```shell
+-------- Start migration --------
+shard-1 is started!!
+shard-2 is started!!
+shard-3 is started!!
+shard-4 is started!!
+shard-5 is started!!
+shard-6 is started!!
+shard-3 is done!
+shard-6 is done!
+shard-4 is done!
+shard-5 is done!
+shard-1 is done!
+shard-2 is done!
+-------- Finish migration --------
+Migration took 48.848333s
+
+
+```
---
+++ shard-concurrency/model.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"gorm.io/gorm"
+)
+
+type Record struct {
+	Hash    string
+	URL     string
+	Login   bool
+	Created int64
+}
+
+func getRecordsFromMySQL(db *gorm.DB, table, lastHash string, limit int) ([]*Record, bool, error) {
+	var records []*Record
+	if err := db.Table(table).Where("hash > ?", lastHash).Find(&records).Limit(limit).Error; err != nil {
+		return nil, false, err
+	}
+
+	return records, len(records) < limit, nil
+}
+
+func insertToDynamo(ctx context.Context, db *dynamodb.Client, records []*Record) error {
+	for _, record := range records {
+		time.Sleep(1 * time.Millisecond) // simulate network latency
+		item, err := attributevalue.MarshalMap(record)
+		if err != nil {
+			return err
+		}
+
+		if _, err := db.PutItem(ctx, &dynamodb.PutItemInput{
+			Item:      item,
+			TableName: aws.String("hash"),
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
