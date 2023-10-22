32dadb122bd82c7f00e605433171cb37cd091f74
Takumaron <tennis.takumax@gmail.com>
2023-10-22 14:11:02 +0700

    Add MySQL and DynamoDB components

---
+++ init.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS `hashdb-1`
+(
+    hash    VARCHAR(64)   NOT NULL,
+    url     VARCHAR(4096) NOT NULL,
+    login   BOOLEAN       NOT NULL,
+    created BIGINT        NOT NULL,
+    PRIMARY KEY (hash)
+);
+
+CREATE TABLE IF NOT EXISTS `hashdb-2`
+(
+    hash    VARCHAR(64)   NOT NULL,
+    url     VARCHAR(4096) NOT NULL,
+    login   BOOLEAN       NOT NULL,
+    created BIGINT        NOT NULL,
+    PRIMARY KEY (hash)
+);
+
+CREATE TABLE IF NOT EXISTS `hashdb-3`
+(
+    hash    VARCHAR(64)   NOT NULL,
+    url     VARCHAR(4096) NOT NULL,
+    login   BOOLEAN       NOT NULL,
+    created BIGINT        NOT NULL,
+    PRIMARY KEY (hash)
+);
+
+CREATE TABLE IF NOT EXISTS `hashdb-4`
+(
+    hash    VARCHAR(64)   NOT NULL,
+    url     VARCHAR(4096) NOT NULL,
+    login   BOOLEAN       NOT NULL,
+    created BIGINT        NOT NULL,
+    PRIMARY KEY (hash)
+);
+
+CREATE TABLE IF NOT EXISTS `hashdb-5`
+(
+    hash    VARCHAR(64)   NOT NULL,
+    url     VARCHAR(4096) NOT NULL,
+    login   BOOLEAN       NOT NULL,
+    created BIGINT        NOT NULL,
+    PRIMARY KEY (hash)
+);
+
+CREATE TABLE IF NOT EXISTS `hashdb-6`
+(
+    hash    VARCHAR(64)   NOT NULL,
+    url     VARCHAR(4096) NOT NULL,
+    login   BOOLEAN       NOT NULL,
+    created BIGINT        NOT NULL,
+    PRIMARY KEY (hash)
+);
